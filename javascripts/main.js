f64815f0731d6837e745f59f42f71dcde55e99bd
ellisthomas <ellisthomas80@gmail.com>
2017-05-14 22:51:50 -0500

    completed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,40 @@
 app.run((FIREBASE_CONFIG) => {
-    firebase.initializeApp(FIREBASE_CONFIG);
+  firebase.initializeApp(FIREBASE_CONFIG);
 });
 
-app.controller("ItemCtrl", ($scope) => {
+app.controller("mushroomCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
+	$scope.mushroomItems = [];
+	$scope.isEdible = "";
+	$scope.searchText = "";
 	
 
+	let getMushroomList = () => {
+		let mushrooms = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+			.then((fbItems) => {
+				let itemCollection = fbItems.data;
+			Object.keys(itemCollection).forEach((key) => {
+				itemCollection[key].id = key;
+				mushrooms.push(itemCollection[key]);
+			});
+			resolve(mushrooms);
+			})
+			.catch((fbError) => {
+			reject(fbError);
+			});
+		});
+	};
 
+	let getItems = () => {
+		getMushroomList().then((itemz) => {
+			$scope.mushroomItems = itemz;
+		}).catch((error) => {
+			console.log("get items error", error);
+		});
+	};
+	getItems();
+	
 
 
 
